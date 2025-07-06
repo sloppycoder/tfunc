4e92580f388740f56c4113f5358153549f19cb14
Li Lin <guru.lin@gmail.com>
2025-07-06 23:35:22 +0800

    fix script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,13 @@
 
 cd "$(dirname "$0")"
 
-set +e
-
-if [ "$DATABASE_URL" = "" && -f [ "/secrets/database_url" ] ]; then
-    export DATABASE_URL=$(cat /secrets/database_url)
+if [ -z "$DATABASE_URL" ] && [ -f /secrets/database_url ]; then
+    DATABASE_URL=$(cat /secrets/database_url)
+    export DATABASE_URL
 fi
 
-if [ "$DATABASE_URL" = "" ]; then
+if [ -z "$DATABASE_URL" ]; then
     echo DATABASE_URL not set, aborting.
-    /bin/sleep infinity
     exit 1
 fi
 
